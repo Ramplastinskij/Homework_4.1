a289ed68e143e5b8be0f08a49775c80f631a117b
Ramplastinskij <roman.zhilkin.21@yandex.ru>
2024-08-14 20:59:12 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Homework_4.1");
+        System.out.println();
+        System.out.println("Задача 1.");
+    }
+
+}
